f5539f05152335430d1eb88777ea6930b17876ec
PrzemekGdula <przemo.g12@wp.pl>
2019-04-02 12:24:00 +0200

    router links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 import Calculators from './Calculators'
 
@@ -7,10 +8,20 @@ import Home from './Home'
 
 const App = () => {
     return (
-        <div>
-            <Home />
-            <Calculators />
-        </div>
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/Calculators">Calculators</Link>
+                    </li>
+                </ul>
+            </div>
+            <Route exact={true} path={'/'} component={Home} />
+            <Route path={'/calculators'} component={Calculators} />
+        </Router>
     )
 }
 
