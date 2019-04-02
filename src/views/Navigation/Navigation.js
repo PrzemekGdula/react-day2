62763a5fa728dcc739c3b6cc7f148c61dd6f0d3b
PrzemekGdula <przemo.g12@wp.pl>
2019-04-02 14:41:35 +0200

    app bar added

--- src/views/Navigation/Navigation.js
+++ src/views/Navigation/Navigation.js
@@ -1,8 +1,32 @@
 import React from 'react'
 
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+
+
 const Navigation = (props) => (
     <div>
-
+        <AppBar position={'static'}
+        >
+            <Toolbar>
+                <IconButton
+                    color={'inherit'}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography
+                    variant={'h6'}
+                    color={'inherit'}
+                    noWrap={true}
+                >
+                    JFDDL7 APP
+                </Typography>
+            </Toolbar>
+        </AppBar>
     </div>
 )
 
