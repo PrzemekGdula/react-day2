b2fe77337d40f39752b487a0e0d674eaf7debe93
PrzemekGdula <przemo.g12@wp.pl>
2019-04-02 10:08:18 +0200

    app component moved to another file

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Name from './Name'
+
+const Header = () => {
+    return (
+        <div>
+            Nazywam sie
+            <Name/>
+        </div>
+    )
+}
+
+export default Header
